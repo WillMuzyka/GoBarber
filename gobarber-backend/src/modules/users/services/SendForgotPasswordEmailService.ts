c47852ea6cbe545a32b44785606ee243e8135f30
WillMuzyka <wmonblog@gmail.com>
2020-05-22 18:31:02 -0300

    Chore: change route underlines to hifen

--- gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -41,7 +41,7 @@ export default class SendForgotPasswordEmailService {
         file: path.resolve(__dirname, '..', 'views', 'forgot_password.hbs'),
         variables: {
           name: user.name,
-          link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
+          link: `${process.env.APP_WEB_URL}/reset-password?token=${token}`,
         },
       },
     });
