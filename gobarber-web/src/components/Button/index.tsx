9bbeef9939f2732db24583606d8cf5b88ca7ad2f
WillMuzyka <wmonblog@gmail.com>
2020-05-23 01:38:04 -0300

    Chore: add rotating svg on buttons

--- gobarber-web/src/components/Button/index.tsx
+++ gobarber-web/src/components/Button/index.tsx
@@ -1,12 +1,22 @@
 import React, { ButtonHTMLAttributes } from 'react';
+import { FiLoader } from 'react-icons/fi';
 
 import { Container } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: number;
+};
 
-const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
-  <Container type="button" {...rest}>
-    {children}
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
+  <Container loading={loading} type="button" {...rest}>
+    {loading ? (
+      <>
+        <FiLoader />
+        <p>Carregando</p>
+      </>
+    ) : (
+      children
+    )}
   </Container>
 );
 
--- gobarber-web/src/components/Button/styles.ts
+++ gobarber-web/src/components/Button/styles.ts
@@ -1,7 +1,20 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import { shade } from 'polished';
 
-export const Container = styled.button`
+interface ButtonProps {
+  loading?: number;
+}
+
+const rotation = keyframes`
+  100% {
+    transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-tranform: rotate(360deg);
+  }
+`;
+
+export const Container = styled.button<ButtonProps>`
   background: #ff9000;
   height: 56px;
   border-radius: 10px;
@@ -11,9 +24,22 @@ export const Container = styled.button`
   font-weight: 500;
   margin-top: 16px;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   transition: background-color 0.2s;
 
   &:hover {
     background-color: ${shade(0.2, '#FF9900')};
+  }
+
+  svg {
+    ${(props) =>
+      !!props.loading &&
+      css`
+        animation: ${rotation} 2s linear infinite;
+      `}
+    margin-right: 8px;
   }
 `;
